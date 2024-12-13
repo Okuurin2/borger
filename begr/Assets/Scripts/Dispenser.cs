ce68f066bcb52b7e3bdffd81ab1ccde58cd35f99
Okuurin2 <s10224981@connect.np.edu.sg>
2024-12-13 23:04:54 +0800

    idk why but i cant fix scene conflict
    >:(((((((((((((((((

--- begr/Assets/Scripts/Dispenser.cs
+++ begr/Assets/Scripts/Dispenser.cs
@@ -40,15 +40,18 @@ public class Dispenser : MonoBehaviour
         {
             Drink drink = cup.GetComponent<Drink>();
             string type = args.interactableObject.transform.gameObject.name;
-            if (colours.TryGetValue(type, out Color color) && drink.typeOfDrink == "" && fillCoroutine == null)
+            if (colours.TryGetValue(type, out Color color) && (drink.typeOfDrink == "" || drink.typeOfDrink == type) && fillCoroutine == null)
             {
                 drink.typeOfDrink = type;
-                cup.GetComponent<Renderer>().material.color = color;
-                fillCoroutine = StartCoroutine(FillCup(args.interactableObject.transform.gameObject.name));
+                Transform circle = cup.transform.Find("Circle");
+                circle.GetComponent<Renderer>().material.color = color;
+                fillCoroutine = StartCoroutine(FillCup());
             }
             else if (type == "Ice")
             {
                 drink.Ice = true;
+                Transform ice = drink.transform.Find("Ice");
+                ice.gameObject.SetActive(true);
             }
             Debug.Log("Pressed");
         }
@@ -64,17 +67,23 @@ public class Dispenser : MonoBehaviour
         }
     }
 
-    private IEnumerator FillCup(string drinkType)
+    private IEnumerator FillCup()
     {
         Drink drink = cup.GetComponent<Drink>();
 
         float fillLevel = drink.fillLevel;
-
+        Transform circle = cup.transform.Find("Circle");
 
         while (fillLevel < 1.0f) 
         {
-            fillLevel += fillRate * Time.deltaTime; 
+            fillLevel += fillRate * Time.deltaTime;     
             drink.fillLevel = Mathf.Clamp01(fillLevel);
+            float size = 0.02215714f - 0.01863194f;
+            size= size* fillLevel + 0.01863194f;
+            float height = 0.00827f + 0.00973f;
+            height = height * fillLevel - 0.00973f;
+            circle.localScale = new Vector3(size, 0.00001f, size);
+            circle.localPosition = new Vector3(0, height, 0);
             yield return null;
         }
     }
